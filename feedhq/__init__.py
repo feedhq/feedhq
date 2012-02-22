ca52a8bcd236722b5349220cffdb5274a493c877
Bruno Renié <buburno@gmail.com>
2012-02-22 21:55:14 +0100

    Enabled packaging

--- feedhq/__init__.py
+++ feedhq/__init__.py
@@ -1,3 +1,10 @@
+VERSION = (0, 1)
+
+def get_version():
+    return ".".join(map(str, VERSION))
+
+__version__ = get_version()
+
 try:
     # don't break setup.py if django hasn't been installed yet
     from django.template.loader import add_to_builtins
---
+++ setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+with open('requirements.txt') as reqs:
+    install_requires = [
+        line for line in reqs.read().split('\n') if (line and not
+                                                     line.startswith('--'))
+    ]
+
+setup(
+    name='feedhq',
+    version=__import__('feedhq').__version__,
+    author='Bruno Renie',
+    author_email='bruno@renie.fr',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/brutasse/feedhq',
+    license='BSD',
+    description='FeedHQ is a web-based feed reader',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    zip_safe=False,
+    install_requires=install_requires,
+)
