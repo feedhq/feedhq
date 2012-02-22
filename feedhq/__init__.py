4a34301f702a5fde1680286f055f8b914af13714
Bruno Renié <buburno@gmail.com>
2012-02-22 23:44:38 +0100

    Timezone support, per-user. Well that was way too easy.

--- feedhq/__init__.py
+++ feedhq/__init__.py
@@ -10,5 +10,6 @@ try:
     from django.template.loader import add_to_builtins
     add_to_builtins('django.templatetags.i18n')
     add_to_builtins('django.templatetags.future')
+    add_to_builtins('django.templatetags.tz')
 except ImportError:
     pass
--- feedhq/profiles/models.py
+++ feedhq/profiles/models.py
@@ -1,11 +1,20 @@
+import pytz
+
 from django.contrib.auth.models import User as DjangoUser
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from ..models import contribute_to_model
+
+TIMEZONES = (
+    (tz, _(tz)) for tz in pytz.all_timezones
+)
 
 
 class User(models.Model):
     username = models.CharField(max_length=75, unique=True)
+    timezone = models.CharField(_('Time zone'), max_length=75,
+                                choices=TIMEZONES, default='UTC')
 
     class Meta:
         abstract = True
