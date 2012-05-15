01e28666320a399cf268d8027840d44327f8f205
Bruno Renié <buburno@gmail.com>
2012-05-15 10:25:21 +0200

    Travis tweaks

--- feedhq/test_settings.py
+++ feedhq/test_settings.py
@@ -1,7 +1,15 @@
 import warnings
 warnings.simplefilter('always')
 
-from settings import *
+from settings import *  # noqa
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'feedhq',
+        'USER': 'postgres',
+    },
+}
 
 TESTS = True
 
