48b69869935b58365e7e0dd99c0694d3d0ae2b3e
Bruno Renié <buburno@gmail.com>
2012-06-27 19:28:00 +0200

    Prevent from closing the connection when in eager mode

--- feedhq/feeds/tasks.py
+++ feedhq/feeds/tasks.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import connection
 
 from ..tasks import raven
@@ -7,11 +8,17 @@ from .utils import FeedUpdater
 @raven
 def update_feed(feed_url, use_etags=True):
     FeedUpdater(feed_url).update(use_etags)
-    connection.close()
+    close_connection()
 
 
 @raven
 def read_later(entry_pk):
     from .models import Entry  # circular imports
     Entry.objects.get(pk=entry_pk).read_later()
-    connection.close()
+    close_connection()
+
+
+def close_connection():
+    """Close the connection only if not in eager mode"""
+    if not settings.RQ.get('eager', True):
+        connection.close()
