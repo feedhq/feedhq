8548362cce44af863a60990b48eefc2e0dca713c
Bruno Renié <buburno@gmail.com>
2012-06-28 09:14:40 +0200

    Don't report feeds as moved if the url is the same

--- feedhq/feeds/models.py
+++ feedhq/feeds/models.py
@@ -157,7 +157,7 @@ class UniqueFeedManager(models.Manager):
 
         obj.failed_attempts = 0
 
-        if response.history:
+        if response.history and obj.url != response.url:
             logger.info("%s moved to %s" % (obj.url, response.url))
             Feed.objects.filter(url=obj.url).update(url=response.url)
             obj.url = response.url
