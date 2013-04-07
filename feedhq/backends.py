1f27a6c8c76e04c89132604b4c89f245b5303975
Bruno Renié <buburno@gmail.com>
2013-04-07 11:55:13 +0200

    Updated the login form to be case-insensitive

--- feedhq/backends.py
+++ feedhq/backends.py
@@ -5,12 +5,23 @@ from django.core.validators import email_re
 from ratelimitbackend.backends import RateLimitMixin
 
 
-class RateLimitMultiBackend(RateLimitMixin, ModelBackend):
+class CaseInsensitiveModelBackend(ModelBackend):
+    def authenticate(self, username, password):
+        try:
+            user = User.objects.get(username__iexact=username)
+        except User.DoesNotExist:
+            return None
+        else:
+            if user.check_password(password):
+                return user
+
+
+class RateLimitMultiBackend(RateLimitMixin, CaseInsensitiveModelBackend):
     """A backend that allows login via username or email, rate-limited."""
     def authenticate(self, username=None, password=None, request=None):
         if email_re.search(username):
             try:
-                username = User.objects.get(email=username).username
+                username = User.objects.get(email__iexact=username).username
             except User.DoesNotExist:
                 pass
         return super(RateLimitMultiBackend, self).authenticate(username,
---
+++ tests/test_backend.py
@@ -0,0 +1,25 @@
+import warnings
+
+from django.test import TestCase
+from django.contrib.auth import authenticate
+
+from .factories import UserFactory
+
+
+class BackendTest(TestCase):
+    def test_case_insensitive_username(self):
+        user = UserFactory.create(username='TeSt')
+
+        with warnings.catch_warnings(record=True) as w:
+            self.assertEqual(authenticate(username='TeSt', password='test').pk,
+                             user.pk)
+
+            self.assertEqual(authenticate(username='test', password='test').pk,
+                             user.pk)
+
+            self.assertEqual(authenticate(username=user.email.lower(),
+                                          password='test').pk, user.pk)
+
+            self.assertEqual(authenticate(username=user.email.upper(),
+                                          password='test').pk, user.pk)
+            self.assertEqual(len(w), 4)
