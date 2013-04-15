f960e3b9ca7fcd704de72f6da8b435adffdce61a
Bruno Renié <buburno@gmail.com>
2013-04-15 20:41:20 +0200

    Removed stuff from the compass config. Apparently still works

--- config.rb
+++ config.rb
@@ -3,9 +3,6 @@ require 'less'
 http_path = '/'
 css_dir = 'feedhq/core/static/core/css'
 sass_dir = 'scss'
-images_dir = 'img'
-images_path = '/static/'
-javascripts_dir = 'js'
 
 output_style = :compressed
 
