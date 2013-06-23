5e0e4c036a9db888b9ce33c788008f794d08606c
Bruno Renié <buburno@gmail.com>
2013-06-23 19:40:34 +0200

    Added FeedFetcher-Google to the UA

--- feedhq/feeds/utils.py
+++ feedhq/feeds/utils.py
@@ -4,6 +4,6 @@ from .. import __version__
 
 USER_AGENT = (
     'FeedHQ/%s (https://github.com/feedhq/feedhq; %%s; https://github.com/'
-    'feedhq/feedhq/wiki/User-Agent)'
+    'feedhq/feedhq/wiki/User-Agent; like FeedFetcher-Google)'
 ) % __version__
 FAVICON_FETCHER = USER_AGENT % 'favicon fetcher'
