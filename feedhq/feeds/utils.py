7f5af1983ea62b3dee3f6243dab58b5cc99fd70e
Bruno Renié <buburno@gmail.com>
2013-06-28 22:21:25 +0200

    Changed the UA not to contain 'User-Agent'
    This triggers bad bot detection on several sites. What the actual fuck…
    http://pastebin.com/5Hw9KZnW

--- feedhq/feeds/utils.py
+++ feedhq/feeds/utils.py
@@ -4,7 +4,7 @@ from .. import __version__
 
 USER_AGENT = (
     'FeedHQ/%s (https://github.com/feedhq/feedhq; %%s; https://github.com/'
-    'feedhq/feedhq/wiki/User-Agent; like FeedFetcher-Google)'
+    'feedhq/feedhq/wiki/fetcher; like FeedFetcher-Google)'
 ) % __version__
 FAVICON_FETCHER = USER_AGENT % 'favicon fetcher'
 
