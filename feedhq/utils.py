94817d569ccde6c18258c18b4f202ea47747a3de
Bruno Renié <brutasse@gmail.com>
2014-02-16 11:03:15 +0100

    Import pool at runtime instead of compile time

--- feedhq/utils.py
+++ feedhq/utils.py
@@ -4,13 +4,12 @@ from django.core.validators import EmailValidator, ValidationError
 
 import redis
 
-from redis_cache.cache import pool
-
 
 def get_redis_connection():
     """
     Helper used for obtain a raw redis client.
     """
+    from redis_cache.cache import pool
     connection_pool = pool.get_connection_pool(
         parser_class=redis.connection.HiredisParser,
         **settings.REDIS)
