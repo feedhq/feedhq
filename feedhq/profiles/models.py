ea558d19e35e4b6ed1caa3e776f149c5e81be0b2
Bruno Renié <brutasse@gmail.com>
2014-04-30 09:27:44 +0200

    Increase bulk timeout

--- feedhq/profiles/models.py
+++ feedhq/profiles/models.py
@@ -203,6 +203,6 @@ class User(PermissionsMixin, AbstractBaseUser):
         for feed in self.feeds.all():
             entries = feed.entries.all()
             docs = [doc.serialize() for doc in entries]
-            es.bulk(docs, index=name, raise_on_error=True)
+            es.bulk(docs, index=name, timeout=60, raise_on_error=True)
         self.es = True
         self.save(update_fields=['es'])
