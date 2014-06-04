c0bb828f120677a0fe12598b1213137ef5ea8906
Bruno Renié <brutasse@gmail.com>
2014-06-04 21:57:28 +0200

    Add elasticsearch debug toolbar panel

--- feedhq/settings.py
+++ feedhq/settings.py
@@ -370,4 +370,20 @@ except ImportError:
 else:
     INSTALLED_APPS += (
         'debug_toolbar',
+        'elastic_panel',
     )
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'elastic_panel.panel.ElasticDebugPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ]
