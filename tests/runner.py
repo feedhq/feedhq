774e52b0d53ad52e9bdbc954cc9e4e92bf007f84
Bruno Renié <brutasse@gmail.com>
2016-08-03 19:13:09 +0200

    travis: bump elastixsearch version, wait for startup

--- tests/runner.py
+++ tests/runner.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.management import call_command
 from django.test.runner import DiscoverRunner
+from elasticsearch.exceptions import NotFoundError
 
 from feedhq import es
 
@@ -8,6 +9,10 @@ from feedhq import es
 class ESTestSuiteRunner(DiscoverRunner):
     def setup_test_environment(self):
         super(ESTestSuiteRunner, self).setup_test_environment()
+        try:
+            es.client.indices.delete(settings.ES_INDEX)
+        except NotFoundError:
+            pass
         call_command('create_index')
         es.wait_for_yellow()
 
